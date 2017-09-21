6582c8b3c9b108086c6ac5f813bd9043756085b9
祥祥 孟 <mengxiangxiang@shengpay.com>
2017-09-21 16:41:49 +0800

    delete target

--- EntManage/entmanage-web/target/entmanage-web/js/self/dateoperate.js
+++
@@ -1,6 +0,0 @@
-function FormatDate (date) {
-		    return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds() ;
-}
-function FormatDateDay (date) {
-    return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate() ;
-}
