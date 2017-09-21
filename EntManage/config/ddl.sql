927d6857bda0ba5b58ee3f7440eca6e58cf6b53d
mengxiangxiang02 <13162757729@163.com>
2017-09-21 22:55:54 +0800

    ss

--- EntManage/config/ddl.sql
+++ EntManage/config/ddl.sql
@@ -36,7 +36,6 @@ CREATE TABLE ent_info (
   GMT_CREATE timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   GMT_MODIFIED timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '数据更新时间',
   PRIMARY KEY (id),
-  UNIQUE KEY idx_ent_info (entname) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='企业信息';
 
 DROP TABLE IF EXISTS class_info;
@@ -48,5 +47,24 @@ CREATE TABLE class_info (
   GMT_CREATE timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   GMT_MODIFIED timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '数据更新时间',
   PRIMARY KEY (id),
-  UNIQUE KEY idx_class_info (classname) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='班级信息';
+
+
+
+DROP TABLE IF EXISTS student_ent_info;
+CREATE TABLE student_ent_info (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  entname varchar(30) NOT NULL COMMENT '企业名称	',
+  entlocation varchar(200) NOT NULL COMMENT '企业地点',
+  entbusiness varchar(500) DEFAULT NULL COMMENT '企业业务',
+  entperson varchar(100) DEFAULT NULL COMMENT '企业人数',
+  enttype varchar(100) DEFAULT NULL COMMENT '企业类型',
+  interviewstudent varchar(100) DEFAULT NULL COMMENT '面试学员',
+  interviewaspect varchar(100) DEFAULT NULL COMMENT '面试方向',
+  studentclass varchar(100) DEFAULT NULL COMMENT '学员班级',
+  entwhite varchar(2) DEFAULT NULL COMMENT ' 是否黑名单企业',
+  remark  varchar(500) DEFAULT NULL COMMENT '备注',
+  GMT_CREATE timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  GMT_MODIFIED timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '数据更新时间',
+  PRIMARY KEY (id),
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='企业信息';
